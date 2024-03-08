0008ff8baef67baf47b43856269e5f683aa3d9d4
anl1313 <fxp671@alumni.ku.dk>
2024-03-08 22:57:35 +0100

    modified:   PS1/problem_set_1.ipynb 	modified:   PS2/mymodule.py 	modified:   PS2/problem_set_2.ipynb

--- PS2/mymodule.py
+++ PS2/mymodule.py
@@ -1,3 +1,17 @@
+import numpy as np
+
 def myfun(n):
     for i in range(n):
         print('hello world!')
+
+vec = np.array([-2.5,-2.0,-1.5,-1.0,-0.5,0,0.5,1.0,1.5,2])
+
+# b. Define probabilities
+prob = np.exp(np.linspace(-2,2,vec.size))**5.6789 # all positive numbers
+prob /= np.sum(prob) # make probabilities sum to one
+
+# c. Get draws from distribution
+x = np.random.choice(vec,size=10**6,p=prob)
+def Rune(x):
+    print(x)
+    
